7d75600e8ddd0d70019a16fd8e2d6564b9411b85
Guille Paz <guille87paz@gmail.com>
2013-06-19 07:02:15 -0300

    #3 Add a parameter to return a copy of the given array.

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 /**
  * Randomize the order of the elements in a given array.
- * @param {arr} arr - The given array.
+ * @param {array} arr - The given array.
+ * @param {boolean} [copy] - Sets if should return a shuffled copy of the given array. By default it's a falsy value.
  * @returns {array}
  */
-module = module.exports = function (arr) {
-    var len = arr.length,
-        random,
-        temp;
+module = module.exports = function (arr, copy) {
 
     if (!Array.isArray(arr)) {
         throw new Error('shuffle-array expect an array as parameter.');
     }
 
+    var aray = arr,
+        len = arr.length,
+        random,
+        temp;
+
+    if (copy === true) {
+        aray = arr.slice();
+    }
+
     while (len) {
         random = Math.floor(Math.random() * len);
         len -= 1;
-        temp = arr[len];
-        arr[len] = arr[random];
-        arr[random] = temp;
+        temp = aray[len];
+        aray[len] = aray[random];
+        aray[random] = temp;
     }
 
-    return arr;
-}
+    return aray;
+};
