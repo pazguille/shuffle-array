4efa473b5fa41772faf53207f3567e592ca2d9f0
Guille Paz <guille87paz@gmail.com>
2013-06-19 09:46:23 -0300

    #4 Add a static method to return a random element of the array.

--- index.js
+++ index.js
@@ -10,22 +10,54 @@ module = module.exports = function (arr, copy) {
         throw new Error('shuffle-array expect an array as parameter.');
     }
 
-    var aray = arr,
+    var collection = arr,
         len = arr.length,
         random,
         temp;
 
     if (copy === true) {
-        aray = arr.slice();
+        collection = arr.slice();
     }
 
     while (len) {
         random = Math.floor(Math.random() * len);
         len -= 1;
-        temp = aray[len];
-        aray[len] = aray[random];
-        aray[random] = temp;
+        temp = collection[len];
+        collection[len] = collection[random];
+        collection[random] = temp;
     }
 
-    return aray;
+    return collection;
+};
+
+/**
+ * Pick one or more random elements from the given array.
+ * @param {array} arr - The given array.
+ * @param {number} picks [optional] - Specifies how many random elements you want to pick. By default it picks 1.
+ * @returns {Object}
+ */
+module.pick = function (arr, picks) {
+
+    if (!Array.isArray(arr)) {
+        throw new Error('shuffle-array.pick() expect an array as parameter.');
+    }
+
+    if (typeof picks === 'number' && picks !== 1) {
+        var len = arr.length,
+            collection = arr.slice(),
+            random = [],
+            index;
+
+        while (picks) {
+            index = Math.floor(Math.random() * len);
+            random.push(collection[index]);
+            collection.splice(index, 1);
+            len -= 1;
+            picks -= 1;
+        }
+
+        return random;
+    }
+
+    return arr[Math.floor(Math.random() * arr.length)];
 };
