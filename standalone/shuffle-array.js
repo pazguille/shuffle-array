0edb5835f95ccb7a4210ed583ba6d27223cf4f47
Guille Paz <guille87paz@gmail.com>
2013-06-19 07:06:47 -0300

    #3 Build standalone version.

--- standalone/shuffle-array.js
+++ standalone/shuffle-array.js
@@ -4,26 +4,32 @@
     /**
      * Randomize the order of the elements in a given array.
      * @param {arr} arr - The given array.
+     * @param {boolean} [copy] - Sets if should return a shuffled copy of the given array. By default it's a falsy value.
      * @returns {array}
      */
-    function shuffle(arr) {
-        var len = arr.length,
+    function shuffle(arr, copy) {
+        if (!Array.isArray(arr)) {
+            throw new Error('shuffle-array expect an array as parameter.');
+        }
+
+        var aray = arr,
+            len = arr.length,
             random,
             temp;
 
-        if (!Array.isArray(arr)) {
-            throw new Error('shuffle-array expect an array as parameter.');
+        if (copy === true) {
+            aray = arr.slice();
         }
 
         while (len) {
             random = Math.floor(Math.random() * len);
             len -= 1;
-            temp = arr[len];
-            arr[len] = arr[random];
-            arr[random] = temp;
+            temp = aray[len];
+            aray[len] = aray[random];
+            aray[random] = temp;
         }
 
-        return arr;
+        return aray;
     }
 
     /**
