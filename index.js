b8ddceb1a0613f61b3721e2b5ac99938be30b785
Guille Paz <guille87paz@gmail.com>
2014-12-30 14:57:36 -0300

    Use an options object instead of parameters.

--- index.js
+++ index.js
@@ -2,23 +2,27 @@
 
 /**
  * Randomize the order of the elements in a given array.
- * @param {array} arr - The given array.
- * @param {boolean} [copy] - Sets if should return a shuffled copy of the given array. By default it's a falsy value.
- * @returns {array}
+ * @param {Array} arr - The given array.
+ * @param {Object} [options] - Optional configuration options.
+ * @param {Boolean} [options.copy] - Sets if should return a shuffled copy of the given array. By default it's a falsy value.
+ * @param {Function} [options.rng] - Specifies a custom random number generator.
+ * @returns {Array}
  */
-function shuffle(arr, copy, rng) {
+function shuffle(arr, options) {
 
   if (!Array.isArray(arr)) {
     throw new Error('shuffle expect an array as parameter.');
   }
 
+  options = options || {};
+
   var collection = arr,
       len = arr.length,
-      rng = rng || Math.random,
+      rng = options.rng || Math.random,
       random,
       temp;
 
-  if (copy === true) {
+  if (options.copy === true) {
     collection = arr.slice();
   }
 
@@ -35,17 +39,22 @@ function shuffle(arr, copy, rng) {
 
 /**
  * Pick one or more random elements from the given array.
- * @param {array} arr - The given array.
- * @param {number} picks [optional] - Specifies how many random elements you want to pick. By default it picks 1.
+ * @param {Array} arr - The given array.
+ * @param {Object} [options] - Optional configuration options.
+ * @param {Number} [options.picks] - Specifies how many random elements you want to pick. By default it picks 1.
+ * @param {Function} [options.rng] - Specifies a custom random number generator.
  * @returns {Object}
  */
-shuffle.pick = function(arr, picks, rng) {
+shuffle.pick = function(arr, options) {
 
   if (!Array.isArray(arr)) {
     throw new Error('shuffle.pick() expect an array as parameter.');
   }
-  
-  rng || (rng = Math.random);
+
+  options = options || {};
+
+  var rng = options.rng || Math.random,
+      picks = options.picks || 1;
 
   if (typeof picks === 'number' && picks !== 1) {
     var len = arr.length,
