22421b5cf72aa60dec19d616cc9ef7737f8e3154
Guille Paz <guille87paz@gmail.com>
2014-12-30 14:58:13 -0300

    Build dist version.

--- dist/shuffle-array.js
+++ dist/shuffle-array.js
@@ -1,5 +1,5 @@
 /*!
- * shuffle-array - v0.0.2
+ * shuffle-array - v0.1.0
  *
  * Copyright (c) 2014, Guille Paz <guille87paz@gmail.com>
  * Released under the MIT license.
@@ -9,27 +9,32 @@
 
 /**
  * Randomize the order of the elements in a given array.
- * @param {array} arr - The given array.
- * @param {boolean} [copy] - Sets if should return a shuffled copy of the given array. By default it's a falsy value.
- * @returns {array}
+ * @param {Array} arr - The given array.
+ * @param {Object} [options] - Optional configuration options.
+ * @param {Boolean} [options.copy] - Sets if should return a shuffled copy of the given array. By default it's a falsy value.
+ * @param {Function} [options.rng] - Specifies a custom random number generator.
+ * @returns {Array}
  */
-function shuffle(arr, copy) {
+function shuffle(arr, options) {
 
   if (!Array.isArray(arr)) {
     throw new Error('shuffle expect an array as parameter.');
   }
 
+  options = options || {};
+
   var collection = arr,
       len = arr.length,
+      rng = options.rng || Math.random,
       random,
       temp;
 
-  if (copy === true) {
+  if (options.copy === true) {
     collection = arr.slice();
   }
 
   while (len) {
-    random = Math.floor(Math.random() * len);
+    random = Math.floor(rng() * len);
     len -= 1;
     temp = collection[len];
     collection[len] = collection[random];
@@ -41,15 +46,22 @@ function shuffle(arr, copy) {
 
 /**
  * Pick one or more random elements from the given array.
- * @param {array} arr - The given array.
- * @param {number} picks [optional] - Specifies how many random elements you want to pick. By default it picks 1.
+ * @param {Array} arr - The given array.
+ * @param {Object} [options] - Optional configuration options.
+ * @param {Number} [options.picks] - Specifies how many random elements you want to pick. By default it picks 1.
+ * @param {Function} [options.rng] - Specifies a custom random number generator.
  * @returns {Object}
  */
-shuffle.pick = function(arr, picks) {
+shuffle.pick = function(arr, options) {
 
   if (!Array.isArray(arr)) {
     throw new Error('shuffle.pick() expect an array as parameter.');
   }
+
+  options = options || {};
+
+  var rng = options.rng || Math.random,
+      picks = options.picks || 1;
 
   if (typeof picks === 'number' && picks !== 1) {
     var len = arr.length,
@@ -58,7 +70,7 @@ shuffle.pick = function(arr, picks) {
         index;
 
     while (picks) {
-      index = Math.floor(Math.random() * len);
+      index = Math.floor(rng() * len);
       random.push(collection[index]);
       collection.splice(index, 1);
       len -= 1;
@@ -68,7 +80,7 @@ shuffle.pick = function(arr, picks) {
     return random;
   }
 
-  return arr[Math.floor(Math.random() * arr.length)];
+  return arr[Math.floor(rng() * arr.length)];
 };
 
 /**
